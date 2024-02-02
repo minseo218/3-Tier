824890164b8d89869bea475d63573494dbe104f5
minseo218 <66362449+minseo218@users.noreply.github.com>
2024-02-02 13:43:46 +0900

    Update say_hi.py

--- script/say_hi.py
+++ script/say_hi.py
@@ -1,15 +1,20 @@
 
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
+from config_reader import read_slack_config
 
-slack_token = "your_app_token"
+# 설정 읽기
+slack_config = read_slack_config()
+slack_token = slack_config['token']
+channel_id = slack_config['channel_id']
+
 # 슬랙 봇과 연결
 client = WebClient(token=slack_token)
 
 
 try:
     response = client.chat_postMessage(
-        channel="your_channel_id", #채널 id를 입력합니다.
+        channel= channel_id, #채널 id를 입력합니다.
         text="안녕하세요~!"
     )
 except SlackApiError as e:
