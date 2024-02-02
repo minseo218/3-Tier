2c93eb9a4efb502870021d5c03a5cff47acdc259
minseo218 <66362449+minseo218@users.noreply.github.com>
2024-02-02 15:06:51 +0900

    Update config_reader.py

--- script/config_reader.py
+++ script/config_reader.py
@@ -1,26 +1,49 @@
 import configparser
 
-def load_config(config_file='config.ini'):
+def load_config(config_file='variable.conf'):
     config = configparser.ConfigParser()
     config.read(config_file)
     return config
 
+def read_slack_config():
+    """Slack 설정 읽기"""
+    config = load_config()
+    return {
+        'token': config.get('common', 'slack_token'),
+        'channel_id': config.get('common', 'channel_id')
+    }
+
+def read_database_config():
+    """데이터베이스 설정 읽기"""
+    config = load_config()
+    return {
+        'db_name': config.get('database', 'db_name'),
+        'db_user': config.get('database', 'db_user'),
+        'db_password': config.get('database', 'db_password'),
+        'db_port': config.get('database', 'db_port'),
+        'db_ip' : config.get('database', 'db_ip')
+    }
+
+def read_remote_config():
+    """원격 서버 설정 읽기"""
+    config = load_config()
+    return {
+        'remote_user': config.get('remote', 'remote_user'),
+        'remote_ip': config.get('remote', 'remote_ip'),
+        'remote_path': config.get('remote', 'remote_path')
+    }
+
+def read_local_config():
+    """로컬 설정 읽기"""
+    config = load_config()
+    return {
+        'backup_path': config.get('local', 'backup_path')
+    }
+
 if __name__ == "__main__":
     config = load_config()
+    slack_config = read_slack_config()
+    database_config = read_database_config()
+    remote_config = read_remote_config()
+    local_config = read_local_config()
 
-    # common 섹션
-    slack_token = config.get('common', 'slack_token')
-    channel_id = config.get('common', 'channel_id')
-
-    # database 섹션
-    db_name = config.get('database', 'db_name')
-    db_user = config.get('database', 'db_user')
-    db_password = config.get('database', 'db_password')
-
-    # remote 섹션
-    remote_user = config.get('remote', 'remote_user')
-    remote_ip = config.get('remote', 'remote_ip')
-    remote_path = config.get('remote', 'remote_path')
-
-    # local 섹션
-    backup_path = config.get('local', 'backup_path')
